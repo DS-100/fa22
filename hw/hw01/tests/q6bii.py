94faf8b71bfb93b9025dd965a834e3fb2ffb414b
Shiangyi Lin <shiangyi.andrea.lin@berkeley.edu>
2022-06-23 15:48:48 -0700

    Remove old hw, discussion code. All lab code should be updated so none is removed. Some old lec code is still up and will be updated as new lectures are released

--- hw/hw01/tests/q6bii.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q6bii',
-    'points': 2,
-    'suites': [{'cases': [{'code': '>>> 0 <= q6bii_r and q6bii_r <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
