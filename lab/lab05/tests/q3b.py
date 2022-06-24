049a52fac84b31ad3e2ee0b4f95a308c30bb7155
Shiangyi Lin <shiangyi.andrea.lin@berkeley.edu>
2022-06-23 20:59:44 -0700

    remove lab 3-12

--- lab/lab05/tests/q3b.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q3b',
-    'points': None,
-    'suites': [{'cases': [{'code': '>>> np.isclose(min_scipy, 2.9982787346405537)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
