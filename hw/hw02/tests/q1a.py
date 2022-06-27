1bdccfcaca016ce3bbbd681497ddf4c54f8d81cb
Shiangyi Lin <shiangyi.andrea.lin@berkeley.edu>
2022-06-26 18:28:32 -0700

    update hw02

--- hw/hw02/tests/q1a.py
+++ hw/hw02/tests/q1a.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q1a',
     'points': 1,
-    'suites': [{'cases': [{'code': '>>> prob_1a >= 0 and prob_1a <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> is_bid_unique or ~is_bid_unique\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- hw/hw02/tests/q3d.py
+++ hw/hw02/tests/q3d.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q3d',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> np.isclose(prob_3d, 1, atol = 1e-4)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'points': 3,
+    'suites': [{'cases': [{'code': '>>> type(ins_missing_score_pivot) == pd.DataFrame\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
