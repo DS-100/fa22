a1663530f9570a7bdf98d952269cd57fd9273aed
Shiangyi Lin <shiangyi.andrea.lin@berkeley.edu>
2022-07-14 10:16:44 -0700

    update proj1B

--- proj/proj1b/ds100_utils.py
+++ proj/proj1b/ds100_utils.py
@@ -103,3 +103,36 @@ def run_linear_regression_test(
             print('Your RMSE loss is: {}'.format(loss))
             return loss
         return loss < threshold
+    
+def run_linear_regression_test_optim(
+    final_model, 
+    process_data_fm, 
+    train_data_path, 
+    test_data_path, 
+    is_test=False, 
+    is_ranking=False,
+    return_predictions=False
+):
+    def rmse(predicted, actual):
+        return np.sqrt(np.mean((actual - predicted)**2))
+
+    training_data = pd.read_csv(train_data_path, index_col='Unnamed: 0')
+    X_train, y_train = process_data_fm(training_data)
+    if is_test:
+        test_data = pd.read_csv(test_data_path, index_col='Unnamed: 0')
+        X_test = process_data_fm(test_data, is_test_set = True)
+        assert len(test_data) == len(X_test), 'You may not remove data points from the test set!'
+
+    final_model.fit(X_train, y_train)
+    if is_test:
+        return final_model.predict(X_test)
+    else:
+        y_predicted = final_model.predict(X_train)
+        loss = rmse(np.exp(y_predicted), np.exp(y_train))
+        if is_ranking:
+            print('Your RMSE loss is: {}'.format(loss))
+            return loss
+        fn = (lambda threshold: loss < threshold)
+        fn.loss = loss
+        fn.signature = (process_data_fm, train_data_path, test_data_path)
+        return fn
